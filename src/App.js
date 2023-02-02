421fd44779fc9cca58ed85954a8fd645a944d7ab
rkpatra201 <docs.rabindrapatra@gmail.com>
2023-02-02 08:52:42 +0530

    add navbar <Link to element/>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import { Navbar } from "./components/Navbar";
 function App() {
   return (
+    <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}>
-        </Route>
-        <Route path="/about" element={<About/>}>
-        </Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
-  )
+    </>
+  );
 }
 
 export default App;
