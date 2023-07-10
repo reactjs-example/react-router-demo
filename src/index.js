59b52cb19c4f28f59c0aca327b0aabc3bf9fbde6
rkpatra201 <docs.rabindrapatra@gmail.com>
2023-07-10 23:42:59 +0530

    react routlet example

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Root } from "./v1/Root";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/v1/Root.js
+++ src/v1/Root.js
@@ -1,20 +1,21 @@
 import { Example1 } from "./components/ex1/Example1";
 import { Example2 } from "./components/ex2/Example2";
 import { Example3 } from "./components/ex3/Example3";
+import { Example4 } from "./components/ex4/Example4";
 
 export const Root = () => {
-
-  let style={
-    margin: 'auto',
-    width: '80%'
-  }  
+  let style = {
+    margin: "auto",
+    width: "80%",
+  };
   return (
     <>
-    <div style={style}>
-    <Example1 />
-      <Example2 />
-      <Example3/>
-    </div>
+      <div style={style}>
+        <Example1 />
+        <Example2 />
+        <Example3 />
+        <Example4/>
+      </div>
     </>
   );
 };
